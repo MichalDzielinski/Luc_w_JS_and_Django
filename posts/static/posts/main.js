e222ff7dcba82d479869683e901deb754d3aa5b7
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-18 11:07:39 +0200

    this day full

---
+++ posts/static/posts/main.js
@@ -0,0 +1,22 @@
+const helloWorldBox = document.getElementById('hello-world')
+
+// helloWorldBox.textContent = 'hello world'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success',response)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error',error);
+    }
+
+})
+
+$.ajax({
+    type: 'GET',
+    url: pass
+
+})
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name= 'posts'
 urlpatterns = [
-    
+    path('', views.post_list_and_create, name='main-board'),
+    path('hello-world/', views.hello_world_view, name='posts-data'),
+    path('data/', views.load_post_data_view, name='hello-world')
 ]

4dac5065b0c3939d96e4cb05250ca7223e057e34
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-20 16:53:16 +0200

    Another

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -2,7 +2,21 @@ console.log('Hi riri')
 
 
 const backBtn = document.getElementById('back-btn')
+const url = window.location.href + "data/"
+const spinnerBox = document.getElementById('spinner-box')
 
 backBtn.addEventListener('click', ()=> {
     history.back()
+})
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log(response)
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error){
+        console.log(error)
+    }
 })

b0a2ddbc99bc69e503a9d5324a6f0e11c4699d74
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-20 15:19:28 +0200

    Some other functionality

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name= 'posts'
 urlpatterns = [
     path('', views.post_list_and_create, name='main-board'),
     path('like-unlike/', views.like_unlike_post, name='like-unlike'),
-    path('hello-world/', views.hello_world_view, name='hello-world'),
+    path('<pk>/', views.post_detail, name='post-detail'),
     path('data/<int:num_posts>/', views.load_post_data_view, name='posts-data')
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -2,33 +2,61 @@ from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
 from django.core import serializers
+from .forms import PostForm
+from profiles.models import Profile
 
 def post_list_and_create(request):
-    qs = Post.objects.all()
+    form = PostForm(request.POST or None)
+    # qs = Post.objects.all()
 
-    context = {'qs': qs}
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+            return JsonResponse({
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.author.user.username,
+                'id': instance.id,
+            })
+
+    context = {'form': form}
     return render(request,'posts/main.html', context)
 
+def post_detail(request, pk):
+    obj = Post.objects.get(pk=pk)
+    form = PostForm()
+
+    context = {'obj': obj, 'form': form}
+
+    return render(request, 'posts/detail.html', context)
+
+
+
+
 def load_post_data_view(request, num_posts): 
-    visible = 3
-    upper = num_posts
-    lower= upper - visible
-    size = Post.objects.all().count()
-    qs = Post.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id': obj.id,
-            'title': obj.title,
-            'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author': obj.author.user.username,
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        visible = 3
+        upper = num_posts
+        lower= upper - visible
+        size = Post.objects.all().count()
+        qs = Post.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username,
 
-        }
+            }
 
-        data.append(item)
-    return JsonResponse({'data': data[lower:upper], 'size': size})
+            data.append(item)
+        return JsonResponse({'data': data[lower:upper], 'size': size})
 
 def like_unlike_post(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -42,6 +70,3 @@ def like_unlike_post(request):
             obj.liked.add(request.user)
         return JsonResponse({'liked': liked, 'count': obj.like_count})
 
-
-def hello_world_view(request):
-    return JsonResponse({'text': 'hello wrld'})
